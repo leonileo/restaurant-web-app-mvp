4003e9fd8215f202fe8d72337f1e9e71232e5c93
leonileo <leolittleprogrammer@gmail.com>
2024-08-24 17:13:03 +0300

    Update: Configure redux provider and added router function.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,35 @@
+// PROJECT POWERD BY LABA CREATIVES
 // index.css
+// Import necessary modules
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './store';
+import './index.css';
+
+// screens
+import HomeScreen from './screens/HomeScreen'
+
+
+// Router function
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />} >
+      <Route index={true} path='/' element={<HomeScreen />} />
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
+
+// PROJECT POWERD BY LABA CREATIVES
