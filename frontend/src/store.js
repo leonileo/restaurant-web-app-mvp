fd1edbcbdba731d0277cbe91d220e3df3292dfb0
leonileo <leolittleprogrammer@gmail.com>
2024-08-24 17:02:58 +0300

    Update: Added redux store and slice.

---
+++ frontend/src/store.js
@@ -0,0 +1,19 @@
+// PROJECT POWERD BY LABA CREATIVES
+// store.js
+// Import necessary modules
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./slices/apiSlice";
+import authSliceReducer from "./slices/authSlice";
+
+const store = configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        auth: authSliceReducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
+});
+
+export default store;
+
+// PROJECT POWERD BY LABA CREATIVES
