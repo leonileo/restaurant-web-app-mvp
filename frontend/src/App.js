df40d858eeccfa0a114f4616daf2e109e2747f00
leonileo <leolittleprogrammer@gmail.com>
2024-08-29 12:20:11 +0300

    Update: removed the header and footer from the common page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,16 @@
 // App.js
 import { ToastContainer } from 'react-toastify'
 import { Outlet } from 'react-router-dom';
-import HeaderComponent from './components/HeaderComponent'
-import FooterComponent from './components/FooterComponent'
 import './App.css';
 
 const App = () => {
   return (
     <>
-      <HeaderComponent />
-          <main className='flex justify-center sm:min-h-[70vh]'>
-              <div className='w-full'>
-                  <Outlet />
-              </div>
-          </main>
-        <FooterComponent/>
+        <main className='flex justify-center sm:min-h-[70vh]'>
+            <div className='w-full'>
+                <Outlet />
+            </div>
+        </main>
       <ToastContainer />
     </>
   );
