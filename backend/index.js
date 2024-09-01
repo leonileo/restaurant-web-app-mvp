38d6fd4fc6f5018b8d4e1bd9ef74d6e55a42f70a
leonileo <leolittleprogrammer@gmail.com>
2024-09-02 01:29:44 +0300

    Update: configure index.js file, connect the db, configured the api routes, error handler

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,59 @@
 // PROJECT POWERD BY LABA CREATIVES
 // index.js
 // Import necessary modules
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv').config();
+const cookieParser = require('cookie-parser')
+const connectDB = require('./config/db.js')
+const accountRoutes = require('./routes/accountRoutes.js')
+const authRoutes = require('./routes/authRoutes.js')
+const foodRoutes = require('./routes/foodRoutes.js')
+const eventRoutes = require('./routes/eventRoutes.js')
+const specialsRoutes = require('./routes/specialsRoutes.js')
+const categoryRoutes = require('./routes/categoryRoutes.js')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware.js')
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 6000
 const ENV = process.env.NODE_ENV || "development"
-
+connectDB()
 // initialize app
 const app = express()
 
+// main function
+app.use('/apiv1/account', accountRoutes)
+app.use('/apiv1/auth', authRoutes)
+app.use('/apiv1/food', foodRoutes)
+app.use('/apiv1/event', eventRoutes)
+app.use('/apiv1/specials', specialsRoutes
+)
+app.use('/apiv1/categories', categoryRoutes
+)
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use('/uploads', express.static(path.join(__dirname,'/uploads')));
 
-// start listening on port 5000
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+// start listening on port 6000
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}, in ${ENV} mode.`)
