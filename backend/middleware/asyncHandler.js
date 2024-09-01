3048c6fce209395b475330504d41df74448e14bb
leonileo <leolittleprogrammer@gmail.com>
2024-09-02 02:41:47 +0300

    Update: added the code for the asynchandler

--- backend/middleware/asyncHandler.js
+++ backend/middleware/asyncHandler.js
@@ -1,4 +1,8 @@
 // PROJECT POWERD BY LABA CREATIVES
 // asyncHandler.js
+const asyncHandler = fn => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+}
 
+module.exports = asyncHandler;
 // PROJECT POWERD BY LABA CREATIVES
