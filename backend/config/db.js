e61c08803d5c8371c1f7c514e5363d3d53781864
leonileo <leolittleprogrammer@gmail.com>
2024-09-01 22:55:52 +0300

    Update: added code to connect the DB.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,17 @@
 // PROJECT POWERD BY LABA CREATIVES
 // db.js
+const mongoose =  require('mongoose')
 
+// connectDB FUNTION
+const connectDB = async () => { 
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`MongoDB conneted: ${conn.connection.host}`)
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit(1)
+    }
+ };
+
+module.exports = connectDB
 // PROJECT POWERD BY LABA CREATIVES
