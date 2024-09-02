9e07b78af6118e79737f30dbf03ac85af3b2232a
leonileo <leolittleprogrammer@gmail.com>
2024-09-02 05:12:00 +0300

    Update: Added code to generate token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,4 +1,21 @@
 // PROJECT POWERD BY LABA CREATIVES
 // generateToken.js
+const jwt = require('jsonwebtoken')
+
+const generateToken = (res, userId) => {
+    const token = jwt.sign( { userId: userId}, process.env.JWT_SECRET, {
+        expiresIn: '10d'
+    });
+
+    // Set JWT as HTTP-Only cookie
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: 30 * 24 * 60 * 60 * 1000
+    })
+}
+
+module.exports = generateToken;
 
 // PROJECT POWERD BY LABA CREATIVES
