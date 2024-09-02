59fc8add6bd4c41789015c0c4b8e4542bdfd2245
leonileo <leolittleprogrammer@gmail.com>
2024-09-02 05:07:48 +0300

    Update: Added the api routes for auth.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,19 @@
 // PROJECT POWERD BY LABA CREATIVES
 // API ROUTE and FUNCTION
 // authRoutes.js
+// Import necessary modules
+const express = require('express');
+const { admin } = require('../middleware/authMiddleware.js');
+const {
+    loginUser,
+    logoutUser
+} = require('../controllers/authController.js');
+const router = express.Router();
 
+router.route('/login')
+.post(admin, loginUser)
+router.route('/logout')
+.post(admin, logoutUser)
+
+module.exports = router
 // PROJECT POWERD BY LABA CREATIVES
