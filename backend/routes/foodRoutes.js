9d3151907bfc7e2733665f83a9b021d2d6dd48b2
leonileo <leolittleprogrammer@gmail.com>
2024-09-02 05:09:16 +0300

    Update: Added api routes for foods

--- backend/routes/foodRoutes.js
+++ backend/routes/foodRoutes.js
@@ -1,5 +1,24 @@
 // PROJECT POWERD BY LABA CREATIVES
 // API ROUTE and FUNCTION
 // foodRoutes.js
+// Import necessary modules
+const express = require('express');
+const {admin} = require('../middleware/authMiddleware.js');
+const {
+    addFood,
+    updateFood,
+    getFoods,
+    deleteFood
+} = require('../controllers/foodController.js');
 
+const router = express.Router();
+
+router.route('/')
+.post(admin, addFood)
+.get(getFoods);
+router.route('/:id')
+.put(admin, updateFood)
+.delete(admin, deleteFood)
+
+module.exports = router
 // PROJECT POWERD BY LABA CREATIVES
