b658e8522f5dc1cd03a0fee9a3a7154b43158e35
leonileo <leolittleprogrammer@gmail.com>
2024-09-02 05:08:26 +0300

    Update: Added the api route for events.

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,5 +1,26 @@
 // PROJECT POWERD BY LABA CREATIVES
 // API ROUTE and FUNCTION
 // eventRoutes.js
+// Import necessary modules
+const express = require('express');
+const {admin} = require('../middleware/authMiddleware.js');
+const {
+    addEvents,
+    getEvents,
+    updateEvent,
+    deleteEvent,
+} = require('../controllers/eventController.js')
+
+const router = express.Router();
+
+router.route('/')
+.post(admin, addEvents)
+.get(getEvents);
+router.route('/:id')
+.put(admin, updateEvent)
+.delete(admin, deleteEvent)
+
+
+module.exports = router
 
 // PROJECT POWERD BY LABA CREATIVES
