73c95d117b234b7a2496e7cd8238363c21826265
leonileo <leolittleprogrammer@gmail.com>
2024-09-02 05:10:09 +0300

    Update: Added api router for special foods.

--- backend/routes/specialsRoutes.js
+++ backend/routes/specialsRoutes.js
@@ -1,5 +1,21 @@
 // PROJECT POWERD BY LABA CREATIVES
 // API ROUTE and FUNCTION
 // specialsRoutes.js
+// Import necessary modules
+const express = require('express');
+const {
+    addSpecial,
+    getSpecials,
+    updateSpecial
+} = require('../controllers/SpecialsController.js')
 
+const router = express.Router();
+
+router.route('/')
+.put(addSpecial)
+.get(getSpecials);
+router.route('/:id')
+.put(updateSpecial)
+
+module.exports = router
 // PROJECT POWERD BY LABA CREATIVES
