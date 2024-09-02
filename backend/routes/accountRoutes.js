80d2426416f4cd25f1c4d0db990bccdb055da84e
leonileo <leolittleprogrammer@gmail.com>
2024-09-02 05:06:48 +0300

    Update: Added the api routes for account.

--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -1,5 +1,21 @@
 // PROJECT POWERD BY LABA CREATIVES
 // API ROUTE and FUNCTION
 // accountRoutes.js
+// Import necessary modules
+const express = require('express');
+const {admin} = require('../middleware/authMiddleware.js');
+const {
+    addUser,
+    getUserAccount,
+    updateUserAccount,
+} = require('../controllers/accountController.js')
 
+const router = express.Router();
+
+router.route('/')
+.post( addUser)
+.get(admin, getUserAccount)
+.put(admin, updateUserAccount)
+
+module.exports = router
 // PROJECT POWERD BY LABA CREATIVES
