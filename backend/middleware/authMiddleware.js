f46a904088d72a34936aee4cb1cdb4a895dc75e0
leonileo <leolittleprogrammer@gmail.com>
2024-09-02 05:02:54 +0300

    Update: Add code for the auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,4 +1,19 @@
 // PROJECT POWERD BY LABA CREATIVES
 // authMiddleware.js
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("./asyncHandler.js");
+const User = require("../models/userModel.js");
 
+
+// Admin middleware
+const admin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error ("Not authorized as admin");
+    }
+}
+
+module.exports = { admin };
 // PROJECT POWERD BY LABA CREATIVES
